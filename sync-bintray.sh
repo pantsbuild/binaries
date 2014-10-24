3fa85a7096be69852b51e11380608995dd58e851
John Sirois <john.sirois@gmail.com>
2014-10-24 15:20:37 -0600

    Fixup typo.

--- sync-bintray.sh
+++ sync-bintray.sh
@@ -58,7 +58,7 @@ echo "Uploading ${count} files to https://dl.bintray.com/pantsbuild/bin"
 echo
 echo "Press CTRL-C at any time to discard the uploaded artifacts; otherwise,"
 echo "the artifacts will be finalized and published en-masse just before the"
-echo "completes."
+echo "script completes."
 echo
 
 for i in $(seq 1 ${count})
