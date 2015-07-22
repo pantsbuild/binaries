b9213ccb01674d11e6b3ff43b2fcd6f8978d2009
John Sirois <john.sirois@gmail.com>
2015-07-22 01:24:57 -0600

    Fixup ragel build script for mac and add the 10.10 OSX binary plus symlinks for other OSX versions.

--- build-ragel-0.6.9.sh
+++ build-ragel-0.6.9.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+if `uname -s | grep -i darwin &> /dev/null`; then
+  unset LDFLAGS
+else
+  LDFLAGS=-static
+fi
+
 curl -k -O https://www.colm.net/files/ragel/ragel-6.9.tar.gz && \
 rm -rf ragel-6.9 && \
 tar -xzf ragel-6.9.tar.gz && \
 cd ragel-6.9 && \
 ./configure && \
-make LDFLAGS=-static
+make LDFLAGS=$LDFLAGS
 
