645acddaeaf5a3cc2a2d03417c08e7b3399652b9
Tejal Desai <tdesai@twitter.com>
2015-07-28 14:52:51 -0700

    Add node binaries

--- build-node-0.12.7.sh
+++ build-node-0.12.7.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-#curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7.tar.gz && \
-#rm -rf node-v0.12.7 && \
-#tar -xzf node-v0.12.7.tar.gz && \
+curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7.tar.gz && \
+rm -rf node-v0.12.7 && \
+tar -xzf node-v0.12.7.tar.gz && \
 cd node-v0.12.7 && \
 ./configure --prefix=./ && make && make install
