8b78a300da31512e18020d00d2421af19dadcefc
Tejal Desai <tdesai@twitter.com>
2015-07-28 16:31:55 -0700

    Upload npm binaries

--- build-node-0.12.7.sh
+++ build-node-0.12.7.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-
-curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7.tar.gz && \
-rm -rf node-v0.12.7 && \
-tar -xzf node-v0.12.7.tar.gz && \
-cd node-v0.12.7 && \
-./configure --prefix=./ && make && make install
+curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x86.tar.gz
+curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz
+curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-darwin-x64.tar.gz
