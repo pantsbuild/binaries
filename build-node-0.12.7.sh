ea277381173b04ee4780c3c26fe7d50d7b94e73b
Tejal Desai <tejal29@gmail.com>
2015-07-29 15:15:30 -0600

    Add build scripts for node module tasks.

---
+++ build-node-0.12.7.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x86.tar.gz
+curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz
+curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-darwin-x64.tar.gz
