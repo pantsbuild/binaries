d6f436f4a2fa07b98d3b711bfdd2615cbf814b67
John Sirois <john.sirois@gmail.com>
2015-08-24 23:15:55 -0600

    Fixup the node tarballs.
    This also adds re-packing into the script for repeatability.

--- build-node-0.12.7.sh
+++ build-node-0.12.7.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
-curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x86.tar.gz
-curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz
-curl -O https://nodejs.org/dist/v0.12.7/node-v0.12.7-darwin-x64.tar.gz
+function build_node() {
+  local version=$1
+  local arch=$2
+  local name="node-${version}-${arch}"
+
+  curl -O https://nodejs.org/dist/${version}/${name}.tar.gz && \
+    rm -rf unpack && mkdir unpack && \
+    tar -xzf ${name}.tar.gz -C unpack && \
+    mv unpack/${name} unpack/node && \
+    tar -czf node.tar.gz -C unpack node/ && \
+    mv node.tar.gz ${name}.tar.gz
+}
+
+ARCHES=(
+  linux-x86
+  linux-x64
+  darwin-x64
+)
+
+for arch in "${ARCHES[@]}"
+do
+  build_node v0.12.7 ${arch}
+done
