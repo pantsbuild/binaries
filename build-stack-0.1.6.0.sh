ba443875b2c2a573d0d4b5f71138e20ba7d8e574
John Sirois <john.sirois@gmail.com>
2015-11-06 15:50:14 -0700

    Fixup stack packaging.

--- build-stack-0.1.6.0.sh
+++ build-stack-0.1.6.0.sh
@@ -8,7 +8,22 @@ ARCHS=(
 
 REV=0.1.6.0
 
+DOWNLOAD_BASE_URL=https://github.com/commercialhaskell/stack/releases/download
+
+function build_stack() {
+  local version=$1
+  local arch=$2
+  local name="stack-${version}-${arch}"
+
+  curl -L -O ${DOWNLOAD_BASE_URL}/v${version}/${name}.tar.gz && \
+    rm -rf unpack && mkdir unpack && \
+    tar -xzf ${name}.tar.gz -C unpack && \
+    mv unpack/${name} unpack/stack && \
+    tar -czf stack.tar.gz -C unpack stack/ && \
+    mv stack.tar.gz ${name}.tar.gz
+}
+
 for arch in ${ARCHS[@]}
 do
-  curl -L -O https://github.com/commercialhaskell/stack/releases/download/v${REV}/stack-${REV}-${arch}.tar.gz
+  build_stack ${REV} ${arch}
 done
