e33bd89d358910cbbacc1b1e12e80bcb4a97205a
mateor <mateorod9@gmail.com>
2016-09-27 23:14:31 -0400

    Add autogen for flex binary and misc thrift 0.9.x script updates.
    * Flex has a bug that affect OSX 10.11 and 10.12, where there
      is an undefined architecture.
    
      They shipped a patch for it in 2.6.0, so this upgrades to that.
      They did not regenerate the libtool files. So this adds an
      autogen step.
    
    * The links were dead as well, so using a sourcefourge mirror now.
    
    * My download of bison included an $pwd/INSTALL file,
      which meant using $pwd/install directory conflicted
      due to case-insensative file system.
    
      This just sets it to INSTALL_PREFIX. In the future there
      could probably be a settings.sh that all these scripts
      sourced and controlled stuff like this.

--- build-thrift-0.9.1.sh
+++ build-thrift-0.9.1.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
 root=$(pwd -P)
+INSTALL_PREFIX=install_root
 
 # The 0.9.1 tarball has hardlinks that fail to extract properly on the /vagrant_data shared
 # folder, using /tmp solves this.
 workdir=$(mktemp -d -t build-thrift-0.9.1.XXXXXX)
 cd ${workdir}
 
-# NB: We need flex for the static link
-curl -O http://iweb.dl.sourceforge.net/project/flex/flex-2.5.39.tar.gz && \
-  rm -rf flex-2.5.39 && \
-  tar -xzf flex-2.5.39.tar.gz && (
-  cd flex-2.5.39 && \
-      sh ./configure --prefix=$(pwd -P)/install && \
-        make install
-  )
+# Upgraded to 2.6.0 to pick up OSX linker fix: https://sourceforge.net/p/flex/bugs/182/
+curl -L -O http://sourceforge.mirrorservice.org/f/fl/flex/flex-2.6.0.tar.gz && \
+rm -rf flex-2.6.0 && \
+tar -xzf flex-2.6.0.tar.gz && (
+  cd flex-2.6.0 && \
+  ./autogen.sh
+  sh ./configure --prefix="$(pwd -P)/$INSTALL_PREFIX" && \
+  make install
+)
 
 # NB: The configure --wthout-* flags just disable building any runtime libs
 # for the generated code.  We only want the codegen side of things.
-curl -O https://www.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz && \
+curl -L -O https://archive.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz && \
 rm -rf thrift-0.9.1 && \
 tar -xzf thrift-0.9.1.tar.gz && \
-LDFLAGS="-all-static -L$(pwd -P)/flex-2.5.39/install/lib" && \
+LDFLAGS="-all-static -L$(pwd -P)/flex-2.6.0/$INSTALL_PREFIX/lib" && \
 cd thrift-0.9.1 && \
 sh ./configure \
   --disable-shared \
