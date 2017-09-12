03886fdc4c4398cd137e7d42f9729a3edb3cb89a
Kris Wilson <kwilson@twitter.com>
2017-09-12 10:47:54 -0700

    Parameterize openssl build.

--- build-watchman.sh
+++ build-watchman.sh
@@ -3,28 +3,9 @@
 set -eo pipefail
 set -x
 
-case $(uname -s) in
-  *Linux*) PLATFORM="linux";
-           ARCH=`uname -p`;
-           SHASUM="sha1sum";
-           OPENSSL_ARCH="linux-x86_64";
-           CFLAGS="-fPIC -fwrapv -O2"
-           ;;
-  *Darwin*) PLATFORM="mac";
-            ARCH=`sw_vers -productVersion | cut -f1,2 -d.`;
-            SHASUM="shasum";
-            OPENSSL_ARCH="darwin64-x86_64-cc";
-            CFLAGS="-fwrapv -Os"
-            ;;
-  *) echo "unsupported platform!"; exit 1;;
-esac
-
 DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 DIRPATH=$(pwd -P)
 BUILD_DIR="watchman_build.${DATE}"
-
-WATCHMAN_DEST_DIR="${DIRPATH}/build-support/bin/watchman/${PLATFORM}/${ARCH}/${WATCHMAN_VERSION}"
-WATCHMAN_VERSION="4.9.0"
 
 PCRE_VERSION="8.41"
 PCRE_SHASUM="dddf0995aefe04cc6267c1448ffef0e7b0560ec0"
@@ -40,6 +21,27 @@ OPENSSL_TARBALL="${OPENSSL_DIR}.tar.gz"
 OPENSSL_URL="https://www.openssl.org/source/${OPENSSL_TARBALL}"
 OPENSSL_INSTALL_DIR="${DIRPATH}/${BUILD_DIR}/openssl_install"
 
+case $(uname -s) in
+  *Linux*) PLATFORM="linux";
+           ARCH=`uname -p`;
+           SHASUM="sha1sum";
+           OPENSSL_ARCH="linux-x86_64";
+           CFLAGS="-fPIC -fwrapv -O2"
+           CXXFLAGS="${CFLAGS} -I${OPENSSL_INSTALL_DIR}/include" LDFLAGS="-L${OPENSSL_INSTALL_DIR}/lib"
+           ;;
+  *Darwin*) PLATFORM="mac";
+            ARCH=`sw_vers -productVersion | cut -f1,2 -d.`;
+            SHASUM="shasum";
+            OPENSSL_ARCH="darwin64-x86_64-cc";
+            CFLAGS="-fwrapv -Os"
+            CXXFLAGS="${CFLAGS}"
+            ;;
+  *) echo "unsupported platform!"; exit 1;;
+esac
+
+WATCHMAN_VERSION="4.9.0"
+WATCHMAN_DEST_DIR="${DIRPATH}/build-support/bin/watchman/${PLATFORM}/${ARCH}/${WATCHMAN_VERSION}"
+
 echo "*****************************"
 echo "building watchman ${WATCHMAN_VERSION}"
 echo "*****************************"
@@ -47,7 +49,7 @@ echo
 
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR
-  # PCRE Build.
+  # PCRE build.
   curl -LO $PCRE_URL
   if [ $($SHASUM $PCRE_TARBALL | grep -c $PCRE_SHASUM) -ne "1" ]; then
     set +x
@@ -61,27 +63,29 @@ pushd $BUILD_DIR
     make install
   popd
 
-  # OpenSSL Build.
-  curl -LO $OPENSSL_URL
-  if [ $($SHASUM $OPENSSL_TARBALL | grep -c $OPENSSL_SHASUM) -ne "1" ]; then
-    set +x
-    echo "openssl checksum invalid! aborting build."
-    exit 1
+  # OpenSSL build (Linux only).
+  if [ "${PLATFORM}" == "linux" ]; then
+    curl -LO $OPENSSL_URL
+    if [ $($SHASUM $OPENSSL_TARBALL | grep -c $OPENSSL_SHASUM) -ne "1" ]; then
+      set +x
+      echo "openssl checksum invalid! aborting build."
+      exit 1
+    fi
+    tar zxf $OPENSSL_TARBALL
+    pushd $OPENSSL_DIR
+      CFLAGS="${CFLAGS}" ./Configure no-shared -fPIC --prefix="${OPENSSL_INSTALL_DIR}" "${OPENSSL_ARCH}"
+      make depend
+      make -j8
+      make install
+    popd
   fi
-  tar zxf $OPENSSL_TARBALL
-  pushd $OPENSSL_DIR
-    CFLAGS="${CFLAGS}" ./Configure no-shared -fPIC --prefix="${OPENSSL_INSTALL_DIR}" "${OPENSSL_ARCH}"
-    make depend
-    make -j8
-    make install
-  popd
 
-  # Watchman Build.
+  # Watchman build.
   git clone https://github.com/facebook/watchman.git watchman
   pushd watchman
     git checkout v${WATCHMAN_VERSION}
     ./autogen.sh
-    CXXFLAGS="${CFLAGS} -I${OPENSSL_INSTALL_DIR}/include" LDFLAGS="-L${OPENSSL_INSTALL_DIR}/lib" ./configure --with-pcre=../pcre_install/bin/pcre-config --disable-statedir --without-python
+    CXXFLAGS="${CXXFLAGS}" ./configure --with-pcre=../pcre_install/bin/pcre-config --disable-statedir --without-python
     make
     mkdir -p $WATCHMAN_DEST_DIR
     cp watchman $WATCHMAN_DEST_DIR/
