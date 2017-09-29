a05cfa96fdeb75cdaed49c037f5f34ff75e4f6f7
Stu Hood <stuhood@twitter.com>
2017-09-29 12:05:01 -0700

    Clone to 3.4.1

--- build-protobuf-3.1.0.sh
+++ build-protobuf-3.1.0.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # NOTE: Ensure you have autoconf, automake and libtool installed via
-#       homebrew/apt-get/whatever.
+        homebrew/apt-get/whatever.
  
 curl -L -O https://github.com/google/protobuf/archive/v3.1.0.tar.gz && \
 rm -rf protobuf-3.1.0 && \
-tar -xzf v3.1.0.tar.gz && \
+tar -xjf v3.1.0.tar.gz && \
 cd protobuf-3.1.0 && \
 ./autogen.sh && \
-./configure --disable-shared && \
+./configure && \
 make
