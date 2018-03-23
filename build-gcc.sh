10e4d82f885b9c8cf2311239be898d6bec548426
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-03-23 03:46:52 -0700

    try revamping the toolchain generation for maintainability

---
+++ build-gcc.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+source ./utils.bash
+
+set_strict_mode
+
+function fetch_extract_gcc_source_release {
+  local -r extracted_dirname="gcc-${GCC_VERSION}"
+  local -r archive_filename="${extracted_dirname}.tar.gz"
+  local -r release_url="https://ftpmirror.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/${archive_filename}"
+
+  local -r downloaded_archive="$(curl_file_with_fail "$release_url" "$archive_filename")"
+  extract_for "$downloaded_archive" "$extracted_dirname"
+}
+
+function build_gcc_with_configure {
+  local -a configure_cmd_line=("$@")
+
+  "${configure_cmd_line[@]}"
+
+  make "-j${MAKE_JOBS}"
+
+  make install
+}
+
+function build_gcc_out_of_tree {
+  local -a configure_args=("$@")
+
+  local -r source_extracted_abs="$(fetch_extract_gcc_source_release)"
+
+  with_pushd >&2 "$source_extracted_abs" \
+                 ./contrib/download_prerequisites
+
+  local -r build_dir_abs="$(mkdirp_absolute_path 'gcc-build')"
+  local -r install_dir_abs="$(mkdirp_absolute_path 'gcc-install')"
+
+  with_pushd >&2 "$build_dir_abs" \
+             build_gcc_with_configure  \
+             "${source_extracted_abs}/configure" \
+             --prefix="$install_dir_abs" \
+             "${configure_args[@]}"
+
+
+  create_gz_package "$install_dir_abs" 'gcc'
+}
+
+function build_osx {
+  build_gcc_out_of_tree \
+    --host='x86_64-apple-darwin' \
+    --target='x86_64-apple-darwin' \
+    "${CONFIGURE_BASE_ARGS[@]}"
+}
+
+function build_linux {
+  build_gcc_out_of_tree \
+    "${CONFIGURE_BASE_ARGS[@]}"
+}
+
+## Interpret arguments and execute build.
+
+readonly TARGET_PLATFORM="$1" GCC_VERSION="$2"
+
+readonly MAKE_JOBS="${MAKE_JOBS:-2}"
+
+readonly SUPPORTED_LANGS='c,c++,objc,obj-c++,fortran'
+
+readonly -a CONFIGURE_BASE_ARGS=(
+  --disable-multilib
+  --enable-languages="${SUPPORTED_LANGS}"
+  --enable-checking='release'
+  --with-pkgversion="Pants-packaged GCC (${GCC_VERSION})"
+  --with-bugurl='https://github.com/pantsbuild/pants/issues'
+)
+
+case "$TARGET_PLATFORM" in
+  osx)
+    with_pushd "$(mkdirp_absolute_path "gcc-${GCC_VERSION}-osx")" \
+               build_osx
+    ;;
+  linux)
+    with_pushd "$(mkdirp_absolute_path "gcc-${GCC_VERSION}-linux")" \
+               build_linux
+    ;;
+  *)
+    die "gcc does not support building for '${TARGET_PLATFORM}'"
+    ;;
+esac
---
+++ utils.bash
@@ -0,0 +1,147 @@
+# Not an executable. Can be sourced by other scripts in this repo for some free
+# error checking for common operations. Uses bash-specific features including
+# `local`.
+
+# see bash(1)
+function set_strict_mode {
+  set -euxo pipefail
+}
+
+# Display a message to stderr.
+function warn {
+  echo >&2 "$@"
+}
+
+# Display a message to stderr, then exit with failure.
+function die {
+  echo >&2 "$@"
+  exit 1
+}
+
+# Display a message read from stdin to stderr, then exit with failure. This can
+# be used with heredocs (<<EOF and friends).
+function die_here {
+  cat >&2
+  exit 1
+}
+
+# Check for the existence of a command, and fail if it's not there. If a command
+# has variations in functionality between Linux and OSX, additional checks may
+# be required.
+function check_cmd_or_err {
+  local -r cmd_name="$1"
+  if ! hash "$cmd_name"; then
+    die_here <<EOF
+The command '${cmd_name}' is required to run this script. You may have to
+install it using your operating system's package manager.
+EOF
+  fi
+}
+
+# Verify that a path exists, and echo the absolute path (not canonical or
+# normalized!).
+function get_existing_absolute_path {
+  local -r path_arg="$1"
+  # -f is an "illegal option" for OSX readlink, so this may have e.g. `/../`
+  # within it.
+  local -r abs_path="$(pwd)/${path_arg}"
+
+  if [[ ! -e "$abs_path" ]]; then
+    die "File at path '${path_arg}' (relative to pwd '$(pwd)') was expected to exist, but does not."
+  fi
+
+  echo "$abs_path"
+}
+
+function var_is_set {
+  local -r var_name="$1"
+  [[ "${!var_name+x}" != '' ]]
+}
+
+# Download a file from a given URL, with verbose output, and exiting with
+# failure on server errors. --fail should probably be on by default, so probably
+# keep that if you add any other curl wrappers.
+function curl_file_with_fail {
+  local -r url="$1" expected_outfile="$2"
+  curl >&2 -L --fail -O "$url"
+  get_existing_absolute_path "$expected_outfile"
+}
+
+# Make a new directory (that may already exist) and echo the absolute path.
+function mkdirp_absolute_path {
+  local -r dir_relpath="$1"
+  mkdir -p "$dir_relpath"
+  get_existing_absolute_path "$dir_relpath"
+}
+
+# TODO: note that expected_output can be any of the extracted entries in the
+# archive, not just a "root dir" or whatever
+function do_extract {
+  local -r archive_path="$1"
+
+  case "$archive_path" in
+    *.tar.xz)
+      check_cmd_or_err 'xz'
+      tar xf "$archive_path"
+      ;;
+    *.tar.gz)
+      tar zxf "$archive_path"
+      ;;
+    *)
+      die "Unrecognized file extension for compressed archive at '${archive_path}'."
+      ;;
+  esac
+}
+
+function extract_for {
+  local -r archive_path="$1"
+  local -a result_path_candidates=("${@:2}")
+
+  do_extract "$archive_path"
+
+  for result_path in "${result_path_candidates[@]}"; do
+    if [[ -e "$result_path" ]]; then
+      get_existing_absolute_path "$result_path"
+      return 0
+    else
+      warn "note: candidate '${result_path}' was not found"
+    fi
+  done
+
+  die "Could not locate the result of extracting '${archive_path}'."
+}
+
+# TODO: mention how you have to might have to quote your globs in args here!!
+function create_gz_package {
+  local -r installed_dir_abs="$1"
+  local -r pkg_name="$2"
+  local -a from_paths=("${@:3}")
+
+  local -r pkg_archive_name="${pkg_name}.tar.gz"
+
+  if [[ "${#from_paths[@]}" -eq 0 ]]; then
+    from_paths=('*')
+  fi
+
+  tar -C "$installed_dir_abs" -czf "$pkg_archive_name" "${from_paths[@]}"
+
+  get_existing_absolute_path "$pkg_archive_name"
+}
+
+function deploy_package_to_dirs_from_stdin {
+  local -r pkg_archive_path="$1"
+  local -r pkg_output_archive_filename="$(basename "$pkg_archive_path")"
+
+  while read dir; do
+    cp >&2 -v "$pkg_archive_path" "${dir}/${pkg_output_archive_filename}"
+  done
+}
+
+function with_pushd {
+  local -r dest="$1"
+  local -a cmd_line=("${@:2}")
+
+  pushd >&2 "$dest"
+  "${cmd_line[@]}"
+  popd >&2
+}
