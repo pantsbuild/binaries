fc4eb0704b33b1e90f83831628c72ab6b53a4ba8
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-04-02 11:10:28 -0700

    remove build.sh for clang 5.0.1 in favor of previous script

--- build-support/bin/clang/linux/x86_64/5.0.1/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yum -y install xz
-
-readonly result="$(./build-clang-for-linux-with-cmake.sh 5.0.1)"
-
-cp "$result" build-support/bin/clang/linux/x86_64/5.0.1/clang.tar.gz
--- build-support/bin/clang/mac/10.13/5.0.1/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-readonly result="$(./build-clang.sh osx 5.0.1)"
-
-cp "$result" build-support/bin/clang/mac/10.13/5.0.1/clang.tar.gz
