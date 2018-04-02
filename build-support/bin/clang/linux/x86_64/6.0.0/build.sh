bf9ea2bd48ae1262d861795408cb308aa372fa44
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-04-02 10:34:57 -0700

    move clang scripts into the appropriate subdir

--- build-support/bin/clang/linux/x86_64/6.0.0/build.sh
+++ build-support/bin/clang/linux/x86_64/6.0.0/build.sh
@@ -4,4 +4,4 @@ yum -y install xz
 
 readonly result="$(./build-clang-for-linux-with-cmake.sh 6.0.0)"
 
-cp "$result" build-support/bin/clang/linux/x86_64/6.0.0/clang.tar.gz
+cp "$result" ./clang.tar.gz
--- build-support/bin/clang/mac/10.13/6.0.0/build.sh
+++ build-support/bin/clang/mac/10.13/6.0.0/build.sh
@@ -2,4 +2,4 @@
 
 readonly result="$(./build-clang.sh osx 6.0.0)"
 
-cp "$result" build-support/bin/clang/mac/10.13/6.0.0/clang.tar.gz
+cp "$result" ./clang.tar.gz
