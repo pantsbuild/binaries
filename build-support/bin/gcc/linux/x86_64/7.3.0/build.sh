fcc9da01ed85e7b251b8cd4f70ff74fca77de182
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-04-02 10:32:31 -0700

    move gcc scripts into the appropriate subdirs

--- build-support/bin/gcc/linux/x86_64/7.3.0/build.sh
+++ build-support/bin/gcc/linux/x86_64/7.3.0/build.sh
@@ -4,4 +4,4 @@ yum -y install wget m4 xz
 
 readonly result="$(./build-gcc.sh linux 7.3.0)"
 
-cp "$result" build-support/bin/gcc/linux/x86_64/7.3.0/gcc.tar.gz
+cp "$result" ./gcc.tar.gz
--- build-support/bin/gcc/mac/10.13/7.3.0/build.sh
+++ build-support/bin/gcc/mac/10.13/7.3.0/build.sh
@@ -2,4 +2,4 @@
 
 readonly result="$(./build-gcc.sh osx 7.3.0)"
 
-cp "$result" build-support/bin/gcc/mac/10.13/7.3.0/gcc.tar.gz
+cp "$result" ./gcc.tar.gz
