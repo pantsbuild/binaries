67517a9a2db9819b8e53543710c916e773d5cb65
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-06-11 23:38:46 -0700

    add xz wrapper script to set the library path

--- build-support/bin/xz/mac/10.13/5.2.4-1/build-xz.sh
+++ build-support/bin/xz/mac/10.13/5.2.4-1/build-xz.sh
@@ -4,11 +4,23 @@ source "$(git rev-parse --show-toplevel)/utils.v1.bash"
 
 set_strict_mode
 
+XZ_WRAPPER_SCRIPT="$(get_existing_absolute_path ./xz.py)"
+
+function wrap_xz_lib_path_then_package {
+  mv bin/xz{,-real}
+
+  cp "$XZ_WRAPPER_SCRIPT" bin/xz
+
+  chmod +x bin/xz
+
+  create_gz_package 'xz'
+}
+
 function package_xz {
   local -r installed_dir_abs="$1"
 
   with_pushd "$installed_dir_abs" \
-             create_gz_package 'xz'
+             wrap_xz_lib_path_then_package
 }
 
 function fetch_extract_xz_source_release {
