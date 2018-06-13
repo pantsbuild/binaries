65f5cd8a52d80e8bfecbe012879c18eb8ffb8e2b
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-06-12 20:51:03 -0700

    add comment and copy to osx

--- build-support/bin/xz/mac/10.13/5.2.4-3/build-xz.sh
+++ build-support/bin/xz/mac/10.13/5.2.4-3/build-xz.sh
@@ -23,12 +23,10 @@ function fetch_extract_xz_source_release {
 function build_xz {
   local -r install_dir_abs="$1"
 
-  # --disable-rpath is necessary to make the xz package work "out of the box" -- otherwise at
-  # runtime it searches for a path in the filesystem of the VM that created it!
-  # We statically link the `xz` executable to avoid conflicts with the installed liblzma.so.
+  # We statically link the `xz` executable to avoid conflicts with any liblzma.so on the host
+  # machine (see pantsbuild/pants#5639).
   ./configure \
-    --disable-rpath \
-    --enable-static \
+    --enable-static --disable-shared \
     --prefix="$install_dir_abs"
 
   make "-j${MAKE_JOBS}"
@@ -43,10 +41,6 @@ function fetch_build_xz {
 
   with_pushd >&2 "$xz_src_extracted_abs" \
                  build_xz "$install_dir_abs"
-
-  local -r static_link_xz="${xz_src_extracted_abs}/src/xz/.libs/xz"
-
-  cp "$static_link_xz" "${install_dir_abs}/bin/xz"
 
   package_xz "$install_dir_abs"
 }
