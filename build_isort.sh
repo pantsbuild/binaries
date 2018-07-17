f08e1f1db77ae649b4de42e68ee5865f0bbb5509
John Sirois <john.sirois@gmail.com>
2018-07-16 22:30:59 -0600

    Upgrade isort to 4.3.4.
    This adds support for more python3 stdlib symbols.
    
    Also introduce a build script.

---
+++ build_isort.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+ROOT="$(git rev-parse --show-toplevel)"
+source "${ROOT}/build_pex.bash"
+
+if (( $# != 1 )); then
+  echo "Usage: $0 <isort version>"
+  exit 1
+fi
+
+version="$1"
+
+target="${ROOT}/build-support/scripts/isort/${version}/isort.pex"
+
+# Two things of note:
+# 1. We force isort to be built for python2.7 since Pants will be running on 2.7.
+# 2. The isort code has a dependency on setuptools but it is not declared, so we do so here.
+run_pex -v \
+  --python=python2.7 \
+  "isort==${version}" \
+  setuptools \
+  -c isort \
+  -o "${target}"
+
+"${target}" --version
+
---
+++ build_pex.bash
@@ -0,0 +1,48 @@
+CACHE_ROOT="${XDG_CACHE_HOME:-$HOME/.cache}/pantsbuild-binaries"
+
+VIRTUALENV_VERSION="16.0.0"
+
+function ensure_venv_installed() {
+  local -r version="${VIRTUALENV_VERSION}"
+
+  local -r venv_cache_dir="${CACHE_ROOT}/virtualenv"
+  local -r venv_dir="${venv_cache_dir}/virtualenv-${version}"
+  if [[ ! -d "${venv_dir}" ]]; then
+    (
+      mkdir -p "${venv_cache_dir}"
+      cd "${venv_cache_dir}"
+      curl -LO https://pypi.python.org/packages/source/v/virtualenv/virtualenv-${version}.tar.gz >&2
+      tar -xvzf "virtualenv-${version}.tar.gz" >&2
+    )
+  fi
+  echo "${venv_dir}"
+}
+
+function activate_venv() {
+  local -r dir="$1"
+
+  local -r activate="${dir}/bin/activate"
+  if [[ ! -f "${activate}" ]]; then
+    mkdir -p "$(dirname "${dir}")"
+
+    (
+    cd "$(ensure_venv_installed)"
+      python2.7 virtualenv.py "${dir}"
+    )
+  fi 
+  source "${activate}"
+}
+
+PEX_VERSION="1.4.4"
+
+function run_pex() {
+  local -r version="${PEX_VERSION}"
+
+  local -r pex_dir="${CACHE_ROOT}/pex/${version}"
+  activate_venv "${pex_dir}"
+  if [[ ! -x "${pex_dir}/bin/pex" ]]; then
+    pip install "pex==${version}" requests
+  fi
+  pex "$@"
+}
+
