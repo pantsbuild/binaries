5c31ede1d5422c98fc6c6f5159d170170939e0fc
Alex Moore <32646775+alexmoore0@users.noreply.github.com>
2018-11-02 14:07:26 -0400

    Adding Node v10.13.0 build scripts for Mac 10.8-10.13 and Linux X64 (#86)

---
+++ build-support/bin/node/linux/x86_64/v10.13.0/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -exu
+
+# Build Node.js for just this version and architecture.
+NODE_VERSION="v10.13.0"
+ARCH="linux-x64"
+
+tarball_name="node-${NODE_VERSION}-${ARCH}"
+
+# Make it easy to use gnu-tar on Mac, since the default bsdtar that comes with OS X 
+# is not compatible with gnu-tar.
+TAR_CMD="${TAR_CMD:-tar}"
+echo "Using tar command '${TAR_CMD}'"
+
+curl -O https://nodejs.org/dist/${NODE_VERSION}/${tarball_name}.tar.gz
+
+# Unpack and repack
+rm -rf unpack && mkdir unpack && \
+${TAR_CMD} -xzf ${tarball_name}.tar.gz -C unpack && \
+rm ${tarball_name}.tar.gz && \
+mv unpack/${tarball_name} unpack/node && \
+${TAR_CMD} -czf node.tar.gz -C unpack node/ && \
+rm -rf unpack
