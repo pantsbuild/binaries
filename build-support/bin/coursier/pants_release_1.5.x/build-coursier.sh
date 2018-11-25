4ee6c43c203d42f584c4c1befacb2a45ffaffccc
Yi Cheng <wisechengyi@gmail.com>
2018-11-24 17:21:23 -0800

    modify

--- build-support/bin/coursier/pants_release_1.5.x/build-coursier.sh
+++ build-support/bin/coursier/pants_release_1.5.x/build-coursier.sh
@@ -6,9 +6,11 @@ VERSION=$1
 DEST_JAR_NAME="coursier-cli-${VERSION}.jar"
 DEST_DIR="build-support/bin/coursier/${VERSION}/"
 
-rm -rf coursier && \
-git clone https://github.com/coursier/coursier.git && \
-pushd coursier && \
+TEMPDIR="/tmp/coursier"
+rm -rf ${TEMPDIR} && \
+mkdir -p ${TEMPDIR} && \
+git clone https://github.com/coursier/coursier.git ${TEMPDIR} && \
+pushd ${TEMPDIR} && \
 git checkout -f ${VERSION} && \
 ./pants binary cli/src/main/scala-2.12:coursier-cli && \
 popd && \
