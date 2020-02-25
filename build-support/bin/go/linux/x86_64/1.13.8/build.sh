e8989eb3eb2afce4374d1d7a7c5ea1db422baf32
Drew Gassaway <dgassaway@twitter.com>
2020-02-25 13:23:59 -0700

    Add go 1.13.8 for linux and mac 10.11+

---
+++ build-support/bin/go/linux/x86_64/1.13.8/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -o xtrace
+curl https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz -o go.tar.gz
---
+++ build-support/bin/go/mac/10.13/1.13.8/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -o xtrace
+curl https://dl.google.com/go/go1.13.8.darwin-amd64.tar.gz -o go.tar.gz
